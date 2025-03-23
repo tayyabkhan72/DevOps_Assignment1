6b4c0bd25f79ba2c630fff913feee4e29e2d1326
Tayyab Naveed <tayyabkhan72>
2025-03-23 19:14:27 +0500

    Normalized line endings to LF

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Home from "./Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
